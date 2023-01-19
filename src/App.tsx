4f2bc75e1e63fe367cd1fee0b2550fdfbafb4fbf
tvviem <trieuvinhviem@gmail.com>
2023-01-19 21:37:10 +0700

    4 - style props React.CSSProperties, passing styles for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import './App.css';
-import { Button } from './components/Button';
-import Input from './components/Input';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log('Button Clicked ', event, id);
-        }}
-      />
-      <Input
-        value=""
-        handleChange={(e) => {
-          console.log('You have typed ' + e.target.value);
+      <Container
+        styles={{
+          border: '1px solid black',
+          padding: '1rem',
+          color: 'red',
+          background: 'white'
         }}
       />
     </div>
