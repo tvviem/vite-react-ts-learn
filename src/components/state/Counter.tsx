aad2e5d304ac3adc1091fd773906289fb3fad08c
tvviem <trieuvinhviem@gmail.com>
2023-01-20 11:23:56 +0700

    7-1 - useReducer strict using Union when declare type

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -3,10 +3,15 @@ import { useReducer } from 'react';
 type CounterState = {
   count: number;
 };
-type CounterAction = {
-  type: string;
+// Type of Logic Actions so complex
+type UpdateAction = {
+  type: 'increment' | 'decrement';
   payload: number;
 };
+type ResetAction = {
+  type: 'reset';
+};
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 1987 };
 
@@ -16,6 +21,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload };
     case 'decrement':
       return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
     default:
       return state;
   }
@@ -34,6 +41,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 };
