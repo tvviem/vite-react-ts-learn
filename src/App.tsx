910a2720ff6a84d2e68fdb2205f438d4b458298f
tvviem <trieuvinhviem@gmail.com>
2023-01-21 23:39:50 +0700

    12 - generic props for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          { id: 1, fName: 'Viem', lName: 'Trieu' },
+          { id: 2, fName: 'Loan', lName: 'P' },
+          { id: 3, fName: 'Baby', lName: 'T' }
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
