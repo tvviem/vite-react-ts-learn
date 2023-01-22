0d401af5420c4af472a83625339ac459b45906dd
tvviem <trieuvinhviem@gmail.com>
2023-01-22 10:05:09 +0700

    reuse react ComponentProps of html elements in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
-import { Toast } from './components/templateliterals/Toast';
+import { CustomButton } from './components/customhtmlelements/Button';
+import { CustomInput } from './components/customhtmlelements/Input';
 
 function App() {
   return (
     <div className="App">
-      <Toast position="center" />
+      <CustomButton
+        variant="primary"
+        style={{
+          color: 'red',
+          background: 'white',
+          marginBottom: '2rem',
+          display: 'block'
+        }}
+        onClick={() => console.log('Clicked')}
+      >
+        Primary Button
+      </CustomButton>
+      <CustomInput
+        placeholder={'this is a customInput component'}
+        style={{ width: '300px' }}
+      />
     </div>
   );
 }
---
+++ src/components/customhtmlelements/Button.tsx
@@ -0,0 +1,13 @@
+// chỉ định nội dung Button chưa là chuỗi
+type ButtonProps = {
+  variant: 'primary' | 'secondary';
+  children: string;
+} & Omit<React.ComponentProps<'button'>, 'children'>;
+
+export const CustomButton = ({ variant, children, ...rest }: ButtonProps) => {
+  return (
+    <button className={`class-with-${variant}`} {...rest}>
+      {children}
+    </button>
+  );
+};
