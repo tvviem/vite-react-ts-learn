bbf00684c78cda5f73e7ea98e3b430df168da046
tvviem <trieuvinhviem@gmail.com>
2023-01-22 10:45:11 +0700

    Polymorphic components using props and generic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
-import { CustomComponent } from './components/customhtmlelements/CustomComponent';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
   return (
     <div className="App">
-      <CustomComponent name={'viem'} isLoggedIn />
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
